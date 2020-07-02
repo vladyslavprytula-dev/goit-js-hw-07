74e25f5048be14af922feafddff4a274ace647c6
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-07-02 13:23:43 +0300

    fixed errors

--- task-1.js
+++ task-1.js
@@ -10,11 +10,18 @@ const categoriesRef = document.querySelector('#categories');
 
 // console.log(`Категория: ${categoriesRef.children[2].firstElementChild.textContent}
 // Количество элементов: ${categoriesRef.children[2].lastElementChild.children.length}`);
+const categoriesRef = document.querySelector('#categories');
 console.log(`В списке ${categoriesRef.children.length} категории.`);
 const liItemRef = categoriesRef.querySelectorAll('li.item');
-console.log(`Категория: ${liItemRef[0].querySelector('h2').textContent}
-Количество элементов: ${liItemRef[0].querySelector('ul').children.length}`);
-console.log(`Категория: ${liItemRef[1].querySelector('h2').textContent}
-Количество элементов: ${liItemRef[1].querySelector('ul').children.length}`);
-console.log(`Категория: ${liItemRef[2].querySelector('h2').textContent}
-Количество элементов: ${liItemRef[2].querySelector('ul').children.length}`);
+
+for (let i = 0; i < liItemRef.length; i += 1) {
+  console.log(`Категория: ${liItemRef[i].querySelector('h2').textContent}
+  Количество элементов: ${liItemRef[i].querySelector('ul').children.length}`);
+}
+
+// console.log(`Категория: ${liItemRef[0].querySelector('h2').textContent}
+// Количество элементов: ${liItemRef[0].querySelector('ul').children.length}`);
+// console.log(`Категория: ${liItemRef[1].querySelector('h2').textContent}
+// Количество элементов: ${liItemRef[1].querySelector('ul').children.length}`);
+// console.log(`Категория: ${liItemRef[2].querySelector('h2').textContent}
+// Количество элементов: ${liItemRef[2].querySelector('ul').children.length}`);
